ab94bffbc8508575853675f1ac6c3ab9f5459cc1
Joseph Walter <joey.walter12@gmail.com>
2026-05-10 21:40:00 -0400

    Add MyProjectGameModeBase class definition

---
+++ Source/MyProject/MyProjectGameModeBase.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyProjectGameModeBase.generated.h"
+
+/**
+ *  Base game mode for MyProject.
+ */
+UCLASS()
+class MYPROJECT_API AMyProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMyProjectGameModeBase();
+};
